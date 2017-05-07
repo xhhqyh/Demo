037bc475bf651816d0bf292d339b19361e52d9af
xhhqyh <2994010023@qq.com>
2017-05-08 00:00:22 +0800

    add design

---
+++ php/magic/log.php
@@ -0,0 +1,9 @@
+PHP魔术方法的使用
+	//将对象的属性进行接轨
+	1.__get / __set 
+	//__call控制php方法调用,__callStatic控制类的静态方法
+	2.__call / __callStatic
+	//将php对象转换为一个字符串
+	3.__toString
+	//将一个php对象当作一个函数执行时，会回调这个魔术方法
+	4.__invoke
